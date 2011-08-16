26f43c46565591e6e55328f7c77f29768cc36fa5
Ian Lawrence <root@ianlawrence.info>
2011-08-16 11:35:41 -0400

    Deleted temporary folders and fixed (another) typo :(

---
+++ assets/intermediate/javascripts/script.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$("#toolsover").hide();$("#minimalistover").hide();$("#minimalist").mouseover(function(a){$("#toolsover").show();$("#minimalistover").show()});$("#minimalist").mouseout(function(a){$("#toolsover").hide();$("#minimalistover").hide()})});
