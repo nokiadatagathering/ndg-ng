b7904c928464994f36c7215d2afc9658c551bf6e
Dominik Frankowski <dominik.frankowski@comarch.com>
2011-08-25 15:35:23 +0200

    example JSON with list
    Signed-off-by: Ian Lawrence <root@ianlawrence.info>

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,13 +1,22 @@
 package controllers;
 
-import play.mvc.*;
-
-//import models.*;
+import java.util.List;
+import play.mvc.Controller;
+import flexjson.JSONSerializer;
+import models.Survey;
 
 public class Application extends Controller {
 
     public static void index() {
         render();
     }
-    
+
+    public static void listSurveys() {
+        List<Survey> surveys =  Survey.findAll();
+        JSONSerializer surveyListSerializer = new JSONSerializer()
+            .include("title","uploadDate","idUser","available","surveyId","ndgUser.username", "resultCollection" )
+                .exclude("*").rootName("surveys");
+        System.out.println(surveyListSerializer.serialize( surveys ) );
+        renderJSON( surveyListSerializer.serialize( surveys ) );
+    }
 }
