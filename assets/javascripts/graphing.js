f5bf3a55068ec1649530bd52ae3cc3cfb959f65d
Ian Lawrence <root@ianlawrence.info>
2011-12-13 10:32:34 -0400

    Adds the flot jquery libraries and draws a pie chart with some fake data

--- assets/javascripts/graphing.js
+++ assets/javascripts/graphing.js
@@ -5,11 +5,70 @@
 
 var Graphing = function() {
 
-    return {showGraph : function() {showGraph();}
-    };
+    return {setupGraph : function() {setupGraph();}
 
-    function showGraph(){
-         alert('Graphing on the way now :)');
+                          };
+
+    function setupGraph(){
+        $('#sectionTitle').text(LOC.get('LOC_GRAPH_VIEW'));
+        $('#searchComboBox').unbind('click');
+        $('#leftColumn').empty('resultListTable');
+        $('#minimalist').empty();
+        $('#search').empty();
+        $('#contentToolbar').hide();
+        setupLeftColumn();
+        prepareGraph();
                         }
 
-}();
+     function prepareGraph() {   
+        $('#minimalist').empty();
+        $('#minimalist').height('400px');
+        $('#minimalist').width('820px');
+
+        // data
+	var data = [
+		{ label: "Question 1",  data: 10},
+		{ label: "Question 2",  data: 30},
+		{ label: "Question 3",  data: 90},
+		{ label: "Question 4",  data: 70},
+		{ label: "Question 5",  data: 80},
+		{ label: "Question 6",  data: 110}
+	];
+	
+	var data = [];
+	var series = Math.floor(Math.random()*10)+1;
+	for( var i = 0; i<series; i++)
+	{
+		data[i] = { label: "Question "+(i+1), data: Math.floor(Math.random()*100)+1 }
+	}
+       
+    
+        // draw the graph
+         $.plot($("#minimalist"), data, 
+	{
+		series: {
+			pie: { 
+				show: true
+			}
+		}
+	});
+        
+        // add some extra content
+        var extraGraphContent = '<div class="itemTextColor">extra graph content</div>';
+        $('#minimalist').append(extraGraphContent);
+                             }
+
+
+    function setupLeftColumn() {
+        var columnContent = '<div class="graphLeftMenu">';
+        columnContent += '<span id ="backHome" class="buttonBack"></span>';
+        columnContent +=  '</div>';
+
+        $('#leftColumn').append( columnContent );
+        $('#backHome').click(function(){ alert("bACK"); } );
+
+                               }
+
+}();                   
+
+
