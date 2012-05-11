aea17d82b6962156dfd32d83b627fb1a53ac8fe6
Rodrigo Ramos <bstritium@gmail.com>
2012-05-11 01:01:49 -0400

    Cleaning up a little
    Signed-off-by: Rodrigo Ramos <bstritium@gmail.com>

--- assets/javascripts/mapping.js
+++ assets/javascripts/mapping.js
@@ -31,15 +31,15 @@ var Mapping = function() {
         getMapData();
     }
 
-      function setupLeftColumn() {
+    function setupLeftColumn() {
         var columnContent = '<div class="resultCollectionQuantity clickableElem">';
             columnContent +='<span id ="resultCollectionQuantityString' + surveyId + '" class="buttonBack"></span>';
             columnContent +='</div>';
 
         $('#leftColumn').append( columnContent );
 
-        $('#resultCollectionQuantityString' + surveyId ).click( surveyId, function() { 
-                               $('.resultListButton').hide(); 
+        $('#resultCollectionQuantityString' + surveyId ).click( surveyId, function() {
+                               $('.resultListButton').hide();
                                $('#map').replaceWith('<table id="minimalist" class="resultListTable"></table>');
                                $('#searchComboBox').show();
                                $('#searchTextField').show();
@@ -47,20 +47,17 @@ var Mapping = function() {
                                                                                     });
     }
 
-
     function getMapData() {
           if( isAllResults ) {
               var contentUrl = 'service/getAllResults?surveyId=' + surveyId;
-              var getJSONQuery = $.getJSON( contentUrl, function(data) {
-                                            createMap(data);
-                                           });
+              var getJSONQuery = $.getJSON( contentUrl, function(data) { createMap(data); });
+
               getJSONQuery.error(Utils.redirectIfUnauthorized);
           }
           else {
                var contentUrl = 'service/getResults?surveyId=' + surveyId + '&resultIDs=' + resultList.join(',');
-               var getJSONQuery = $.getJSON( contentUrl, function(data) {
-                                             createMap(data);
-                                            });
+               var getJSONQuery = $.getJSON( contentUrl, function(data) { createMap(data); });
+
                getJSONQuery.error(Utils.redirectIfUnauthorized);
           }
     }
@@ -71,22 +68,18 @@ var Mapping = function() {
         $('#datatable').width('810px');
 
         $('#datatable').empty();
-       
-        
+
         var mapDiv = '<div id="map" style="z-index: -1; width: 100%; height: 100%;"></div>'
         $('#datatable').append(mapDiv);
  
-        
         nokia.Settings.set( "appId", "7s7qhRnP7jg2SK34tw7Y");
         nokia.Settings.set( "authenticationToken", "x353yEXgA8JNbPs2tDtM7g");
 
         var mapContainer = document.getElementById("map");
-
         var infoBubbles = new nokia.maps.map.component.InfoBubbles();
 
         var startLat = parseFloat(data.items[0].latitude);
         var startLong = parseFloat(data.items[0].longitude);
-
 
         var map = new nokia.maps.map.Display(mapContainer, {
 	        center: [startLat, startLong],
@@ -96,18 +89,16 @@ var Mapping = function() {
 		          new nokia.maps.map.component.ZoomBar(),
 		          new nokia.maps.map.component.Behavior(),
 		          new nokia.maps.map.component.TypeSelector(),
-		          new nokia.maps.map.component.Traffic(),
-		          new nokia.maps.map.component.PublicTransport(),
 		          new nokia.maps.map.component.Overview(),
 		          new nokia.maps.map.component.ScaleBar(),
 		          new nokia.maps.positioning.component.Positioning(),
 		          //new nokia.maps.map.component.ContextMenu()
             ]
-        });  
-        
+        });
+
         map.removeComponent(map.getComponentById("zoom.MouseWheel"));
        // console.log(nokia.maps.Features.getFeatureMap());
-      
+
         $.each(data.items,function(i,item) {
             var standardMarker = new nokia.maps.map.StandardMarker([parseFloat(item.latitude), parseFloat(item.longitude)], {
                 text: i + 1,
@@ -124,7 +115,7 @@ var Mapping = function() {
             var html = "<div>" +
                     "<h2>Result information</h2>" +
                     "<br/><h3 style=\"color:white;\">Title: " + item.title      + "</h3>" +
-                    "<br/><h3 style=\"color:white;\">End: "   + endTime.toDateString() + " " + endTime.toTimeString()+ "</h3>" +
+                    "<br/><h3 style=\"color:white;\">Created: "   + endTime.toDateString() + " " + endTime.toTimeString()+ "</h3>" +
                     "<br/><h3 style=\"color:white;\">Coord: " + item.latitude   + " " + item.longitude + "</h3></div>";
 
             standardMarker.addListener(
@@ -136,7 +127,4 @@ var Mapping = function() {
             map.objects.add(standardMarker);
         });
     }
-
- 
-
 }();
