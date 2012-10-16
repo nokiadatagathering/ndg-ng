79756bac6bac28928865af2c78fb87f6525565f7
Rodrigo Ramos <bstritium@gmail.com>
2012-10-16 16:43:28 -0400

    Geopoint: Adding evolution to update current database and support the new geopoint type
    Signed-off-by: Rodrigo Ramos <bstritium@gmail.com>

---
+++ db/evolutions/1.sql
@@ -0,0 +1,11 @@
+# Update question_type
+ 
+# --- !Ups
+UPDATE question_type
+SET supported=1
+WHERE type_name='geopoint';
+ 
+# --- !Downs
+UPDATE question_type
+SET supported=0
+WHERE type_name='geopoint';
