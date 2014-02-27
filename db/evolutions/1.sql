7e4800db69e6201e484f73813f7296f1474a027b
Ian Lawrence <ian@codezon.com>
2014-02-27 10:52:02 -0300

    Jobs table is created automatically so no need for a revolution

--- db/evolutions/1.sql
+++ db/evolutions/1.sql
@@ -6,20 +6,11 @@ UPDATE question_type
 SET supported=1
 WHERE type_name='geopoint';
 
-CREATE TABLE jobs (
-    id bigint(20) NOT NULL AUTO_INCREMENT,
-    surveyId varchar(255) NOT NULL,
-    dateTo varchar(255) NOT NULL,
-    dateFrom varchar(255) NOT NULL,
-    email varchar(255) NOT NULL,
-    complete boolean NOT NULL,
-    PRIMARY KEY (id)
-);
  
 # --- !Downs
 UPDATE question_type
 SET supported=0
 WHERE type_name='geopoint';
 
-DROP TABLE jobs;
+
 
